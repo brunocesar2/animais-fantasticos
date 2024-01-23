f92ceee2be504a899b90af237a6c536e872553c8
Bruno César <brunocesar.tenorio1@gmail.com>
2024-01-23 13:47:47 -0300

    updating number animation

--- js/modules/number-animation.js
+++ js/modules/number-animation.js
@@ -1,20 +1,27 @@
 export default function initNumberAnimation() {
-  const numbers = document.querySelectorAll("[data-number] span");
-  numbers.forEach((number) => {
-    const total = +number.innerHTML;
-    const increment = Math.floor(total / 100);
-    let start = 0;
-    const timer = setInterval(() => {
-      start = start + increment;
-      number.innerHTML = start;
-      if (start > total) {
-        number.innerHTML = total;
-        clearInterval(timer);
-      }
-    }, 25 * Math.random());
-  });
-  function handleMutation() {}
+  function numberAnimation() {
+    const numbers = document.querySelectorAll("[data-number] span");
+    numbers.forEach((number) => {
+      const total = +number.innerText;
+      const increment = Math.floor(total / 100);
+      let start = 0;
+      const timer = setInterval(() => {
+        start = start + increment;
+        number.innerText = start;
+        if (start > total) {
+          number.innerText = total;
+          clearInterval(timer);
+        }
+      }, 25 * Math.random());
+    });
+  }
+  function handleMutation(mutation) {
+    if (mutation[0].target.classList.contains("active")) {
+      observer.disconnect();
+      numberAnimation();
+    }
+  }
   const observerTarget = document.querySelector(".numbers");
   const observer = new MutationObserver(handleMutation);
-  observer.observer(observerTarget, { attributes: true });
+  observer.observe(observerTarget, { attributes: true });
 }
